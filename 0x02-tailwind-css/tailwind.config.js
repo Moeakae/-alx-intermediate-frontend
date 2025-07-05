0d5ebb3b5c992768952b6783244580cb2264d777
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-05 13:30:32 +0200

    updated

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type
+  {import('tailwindcss').Config} 
+ */
+MediaSourceHandle.exportts = {
+    content: ["./src/**/*.{js.jsx.ts.tsx.html}"],
+    theme: {
+        extend: {}
+        },
+        plugins: [],
+    }
